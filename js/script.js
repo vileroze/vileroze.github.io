e2d668589c1accea33ded32828e519f38ded1a2e
vileroze <50352881+vileroze@users.noreply.github.com>
2020-05-12 17:28:43 +0545

    added files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* open subscribe page */
+function subscribe()
+{
+	document.querySelector('.pop-up').style.display = 'flex';
+}
+
+/* close subscribe page */
+function close_pop_up()
+{
+	document.querySelector('.pop-up').style.display = 'none';
+}
+
