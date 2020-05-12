72816b49318be6537143d191ffab68b36045b792
vileroze <50352881+vileroze@users.noreply.github.com>
2020-05-12 21:05:40 +0545

    updated

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -10,3 +10,13 @@ function close_pop_up()
 	document.querySelector('.pop-up').style.display = 'none';
 }
 
+/* open reserve page */
+function reserve()
+{
+	document.querySelector('.reserve').style.display = 'flex';
+}
+
+function close_reserve()
+{
+	document.querySelector('.reserve').style.display = 'none';
+}
