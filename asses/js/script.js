fbc2f1f66b159c6bdafea6ff38509bf4143d4c98
vileroze <50352881+vileroze@users.noreply.github.com>
2020-05-18 17:42:31 +0545

    updatess

--- asses/js/script.js
+++ asses/js/script.js
@@ -1,3 +1,5 @@
+
+
 /* open subscribe page */
 function subscribe()
 {
@@ -14,9 +16,47 @@ function close_pop_up()
 function reserve()
 {
 	document.querySelector('.reserve').style.display = 'flex';
+
+
 }
 
 function close_reserve()
 {
 	document.querySelector('.reserve').style.display = 'none';
 }
+
+
+
+/*reserve form validation */
+function validate()
+{
+	var name = document.getElementById("reserve_name");
+	var people = document.getElementById("reserve_people");
+	var date = document.getElementById("reserve_date");
+	var now = new Date();
+
+	if (name.value == "" || name.value == null) {
+		alert("name field cannot be blank");
+		return false;
+	}
+
+	else if (people.value == "") {
+		alert("number of pople field cannot be blank");
+		return false;
+	}
+
+	else if (people.value < 1){
+		alert("number of pople cannot be less than one");
+		return false;
+	}
+
+	else
+	{
+		alert("YOUR RESERVATION HAS BEEN BOOKED !!!");
+		true;
+	}
+}
+
+
+
+
