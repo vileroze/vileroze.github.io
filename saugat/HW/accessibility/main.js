102de3d1f72648d0f86927888738b6f36a209d70
vileroze <50352881+vileroze@users.noreply.github.com>
2020-06-06 10:01:42 +0545

    hw

---
+++ saugat/HW/accessibility/main.js
@@ -0,0 +1,67 @@
+// functionality for showing/hiding the comments section
+
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
+}
+
+document.onkeydown = function(e){
+  if (e.keyCode == 13){
+    document.activeElement.click();
+  }
+}
+
+const transcript = document.querySelector('.transcript');
+const transcriptBtn = document.querySelector('.trans-control button');
+
+transcriptBtn.onclick = function(){
+  if (transcriptBtn.textContent === 'Show Transcript'){
+    transcript.style.height = '50px';
+    transcriptBtn.textContent = 'Hide Transcript';
+  }
+  else{
+    transcript.style.height = '0';
+    transcriptBtn.textContent = 'Show Transcript';
+  }
+}
