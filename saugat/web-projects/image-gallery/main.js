3381e9399854503bd8656ca917fc4f41691f891c
vileroze <50352881+vileroze@users.noreply.github.com>
2020-06-20 09:35:08 +0545

    update meow

---
+++ saugat/web-projects/image-gallery/main.js
@@ -0,0 +1,32 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+var i;
+for (i = 1; i < 6; i++) {
+  const newImage = document.createElement('img');
+	newImage.setAttribute('src', 'images/pic'+ i + '.jpg');
+	thumbBar.appendChild(newImage);
+	newImage.onclick = function(e) {
+	displayedImage.src = e.target.src;
+	}
+}
+
+/* Wiring up the Darken/Lighten button */
+btn.onclick = function()
+{
+	const btnClass = btn.getAttribute('class');
+	if ( btnClass === 'dark'){
+		btn.setAttribute('class', 'light');
+		btn.textContent = 'lighten';
+		overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+	}
+	else{
+		btn.setAttribute('class', 'dark');
+		btn.textContent = 'darken';
+		overlay.style.backgroundColor = "rgba(0,0,0,0)";
+	}
+}
