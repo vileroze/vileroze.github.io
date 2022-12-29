e44b4f8b62b789afe1be3e44e1fbd84d83837aaa
vileroze <50352881+vileroze@users.noreply.github.com>
2022-12-29 17:10:42 +0545

    changed validation in validation.js

--- CODE_PIXELZ_INTERNSHIP/DAY-4/validation.js
+++ CODE_PIXELZ_INTERNSHIP/DAY-4/validation.js
@@ -12,36 +12,47 @@ function validate(){
     let u_email = document.getElementById("email").value;
     let u_phone = document.getElementById("phone").value;
     let u_website = document.getElementById("website").value;
+    let err_prefix = "Error: ";
+    let err_msg = document.getElementById("err_msg");
 
     //check for empty entries
-    if(u_name === "" || u_email === "" || u_phone === "" || u_website === ""){
+    if(u_name != "" || u_email != "" || u_phone != "" || u_website != ""){
+
+        //validate email address usign regex
+        if(u_email.match(email_regex)){
+            //validate phone number
+            if(u_phone.length === 10){
+                //check for valid url
+                try {
+                    let u_url = new URL(u_website);
+                    document.getElementById("sbt-btn").classList.remove("disabled"); //button enabled
+                } catch (error) {
+                    document.getElementById("sbt-btn").classList.add("disabled");
+                    err_msg.innerHTML = err_prefix + "Try adding https:// or http:// in the begining !!";
+                }
+            }else{
+                document.getElementById("sbt-btn").classList.add("disabled");
+                err_msg.innerHTML = err_prefix + "Phone number should be of 10 digits !!";
+            }
+        }else{
+            document.getElementById("sbt-btn").classList.add("disabled");
+            err_msg.innerHTML = err_prefix + "Invalid email!!";
+        }
+        
+    }else{
         document.getElementById("sbt-btn").classList.add("disabled");
-        return alert("All fields must be filled !!");
+        err_msg.innerHTML = err_prefix + "All fields must be filled !!";
     }
 
-    //validate email address usign regex
-    if(!u_email.match(email_regex)){
-        document.getElementById("sbt-btn").classList.add("disabled");
-        return alert("Invalid email!!");
-    }
+    
 
-    //validate phone number
-    if(u_phone.length < 10 || u_phone.length > 10){
-        document.getElementById("sbt-btn").classList.add("disabled");
-        return alert("Phone number should be of 10 digits!!");
-    }
+    
 
-    //check for valid url
-    try {
-        let u_url = new URL(u_website);
-    } catch (error) {
-        document.getElementById("sbt-btn").classList.add("disabled");
-        return alert("Try adding https:// or http:// in the begining");
-    }
+    
 
-    //if all above condition passed
-    alert("Form is ready to SUBMIT!!");
-    document.getElementById("sbt-btn").classList.remove("disabled"); //button enabled
+    // //if all above condition passed
+    // alert("Form is ready to SUBMIT!!");
+    // document.getElementById("sbt-btn").classList.remove("disabled"); //button enabled
 }
 
 
